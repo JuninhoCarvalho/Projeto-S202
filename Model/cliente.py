29cbbb63196190918286cc094a6727efed4299d1
JuninhoCarvalho <juninho.maglioni@gmail.com>
2022-06-19 19:22:32 -0300

    removendo pasta model

--- Model/cliente.py
+++
@@ -1,8 +0,0 @@
-import pessoa
-
-class Cliente(pessoa.Pessoa):
-    def __init__(self, nome, cpf, dataCompra):
-        super().__init__(nome, cpf)
-    
-    def compraCarro():
-        print("comprou um carro")
